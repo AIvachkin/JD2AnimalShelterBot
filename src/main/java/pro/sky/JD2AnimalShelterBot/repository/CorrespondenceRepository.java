92c2e5720464a71b1a48453c6064225fb3b1e0e8
Webkonditer <webkonditer@yandex.ru>
2022-11-29 17:50:09 +0300

    Добавлены репозитории к новым сущностям.

--- src/main/java/pro/sky/JD2AnimalShelterBot/repository/CorrespondenceRepository.java
+++ src/main/java/pro/sky/JD2AnimalShelterBot/repository/CorrespondenceRepository.java
@@ -2,7 +2,12 @@ package pro.sky.JD2AnimalShelterBot.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 import pro.sky.JD2AnimalShelterBot.model.Correspondence;
 
+/**
+ * Интерфейс работы с БД переписки
+ */
+@Repository
 public interface CorrespondenceRepository extends CrudRepository<Correspondence, Long> {
 }
