be6162f1747cabb443d844cdc51661379bdcf813
Денис <den_2005@list.ru>
2022-12-02 01:50:40 +0300

    добавил таблицы correspondence и trustees_reports

--- src/main/resources/liquibase/scripts/changesets.sql
+++ src/main/resources/liquibase/scripts/changesets.sql
@@ -5,8 +5,11 @@ CREATE TABLE pet (
     pet_id BIGINT,
     name VARCHAR,
     age INT,
-    user_id BIGINT
+    user_id BIGINT,
+    probation_period_up_to DATE,
+    fixed BOOLEAN
 );
+
 CREATE TABLE user_data_table (
     chat_id BIGINT,
     firstname VARCHAR,
@@ -22,4 +25,24 @@ CREATE TABLE volunteer (
 CREATE TABLE report (
     rep_id BIGINT,
     report VARCHAR
+);
+
+-- changeset denis:2
+CREATE TABLE correspondence (
+    message_id BIGINT,
+    chat_id BIGINT,
+    date_time TIMESTAMP,
+    text TEXT,
+    answered BOOLEAN,
+    who_sent_it VARCHAR
+);
+-- changeset denis:3
+CREATE TABLE trustees_reports (
+    message_id BIGINT,
+    chat_id BIGINT,
+    pet_id BIGINT,
+    date_time TIMESTAMP,
+    photo VARCHAR,
+    text TEXT,
+    viewed BOOLEAN
 );
